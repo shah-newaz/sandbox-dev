e855ab50dab8d83d58f505afac8b1d774e2ce045
Shah <rifat_rifats@live.com>
2017-10-24 17:20:21 +0600

    Revert "Authentication"
    This reverts commit 02c225a8fd15a397a0486325cae9301468dab62d.

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,14 @@
 
 Route::get("/", "SandboxController@getSandbox")->name("home");
 
-Route::middleware('auth')->group(function () {
+Route::get("/products", "ProductsController@index")->name("product.index");
+Route::get("/product/new", "ProductsController@form")->name("product.new");
+Route::get("/product/edit/{product}", "ProductsController@form")->name("product.edit");
+Route::post("/product/delete/{product}", "ProductsController@delete")->name("product.delete");
+Route::post("/product/new", "ProductsController@post")->name("product.save");
 
-	Route::get("/products", "ProductsController@index")->name("product.index");
-	Route::get("/product/new", "ProductsController@form")->name("product.new");
-	Route::get("/product/edit/{product}", "ProductsController@form")->name("product.edit");
-	Route::post("/product/delete/{product}", "ProductsController@delete")->name("product.delete");
-	Route::post("/product/new", "ProductsController@post")->name("product.save");
-
-	Route::get("/categories", "CategoriesController@index")->name("category.index");
-	Route::get("/category/new", "CategoriesController@form")->name("category.new");
-	Route::get("/category/edit/{category}", "CategoriesController@form")->name("category.edit");
-	Route::post("/category/delete/{category}", "CategoriesController@delete")->name("category.delete");
-	Route::post("/category/new", "CategoriesController@post")->name("category.save");
-
-});
-
-
-
-Route::get('/register', 'UsersController@registerForm')->name('user.register');
-Route::post('/register', 'UsersController@register')->name('user.register.post');
-
-Route::get('/login', 'UsersController@loginForm')->name('login');
-Route::get('/logout', 'UsersController@logout')->name('user.logout');
-Route::post('/login', 'UsersController@login')->name('user.login.post');
+Route::get("/categories", "CategoriesController@index")->name("category.index");
+Route::get("/category/new", "CategoriesController@form")->name("category.new");
+Route::get("/category/edit/{category}", "CategoriesController@form")->name("category.edit");
+Route::post("/category/delete/{category}", "CategoriesController@delete")->name("category.delete");
+Route::post("/category/new", "CategoriesController@post")->name("category.save");
